007d9862d271246aaf1d4691e1bb0ee503fd48ff
Matt Cockayne <matt@phpboyscout.uk>
2020-10-08 11:04:11 +0100

    bumps version number

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def readme():
 
 setup(name='ncpa-nvidiasmi-plugin',
         python_requires='>3.5.2',
-        version="0.1.0",
+        version="0.2.0",
         author='Matt Cockayne',
         author_email='matt@phpboyscout.uk',
         license="MIT",
